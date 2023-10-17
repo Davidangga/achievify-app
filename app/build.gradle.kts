fcb63bf6a11425adbea96ffbbe745ba646d89346
davidangga <davidanggadeveloper@gmail.com>
2023-10-17 18:14:08 +1100

    progress on implmentigng figma design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
--- app/src/main/java/com/example/achivify/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/achivify/ui/view/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.achivify.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.achivify.R
 import com.example.achivify.databinding.ActivityMainBinding
 
@@ -16,21 +19,45 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // initialise binding mechanism
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // change action bar into toolbar and make hamburger icon
         setSupportActionBar(binding.toolbar)
         supportActionBar?.title = null
-
         val toggle = ActionBarDrawerToggle(this, binding.drawerLayout,binding.toolbar,R.string.open_drawer, R.string.close_drawer)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        // listener for back button when drawer activate
         val backButton = binding.navigationView.getHeaderView(0).findViewById<ImageView>(R.id.back_button)
         backButton.setOnClickListener{
             binding.drawerLayout.closeDrawer(GravityCompat.START)
         }
 
+        binding.navigationView.setNavigationItemSelectedListener {
+            item: MenuItem ->
+            var fragment: Fragment? = null
+            when (item.itemId) {
+                R.id.nav_todo -> fragment = TodoListFragment()
+                R.id.nav_achievement -> fragment = AchievementsFragment()
+                R.id.nav_explore -> fragment = ExploreFragment()
+                else -> fragment = TodoListFragment()
+            }
+            if(fragment != null){
+                val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+                ft.replace(R.id.fragment_container, fragment)
+                ft.commit()
+                binding.drawerLayout.closeDrawers()
+                true
+            }
+            else{
+                false
+            }
+
+        }
     }
 
     override fun onBackPressed() {
